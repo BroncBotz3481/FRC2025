4eac8300261209d6daf6022e0ec9960a32bee87a
thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>
2025-01-29 23:58:27 -0600

    Added sim stuff for the wrist and roller.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,68 +7,92 @@ package frc.robot;
 import edu.wpi.first.math.util.Units;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean constants. This
+ * class should not be used for any other purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-    public static final int kOperatorControllerPort = 1;
-    public static final double DEADBAND = 0.05;
+public final class Constants
+{
+
+  public static class OperatorConstants
+  {
+
+    public static final int    kDriverControllerPort   = 0;
+    public static final int    kOperatorControllerPort = 1;
+    public static final double DEADBAND                = 0.05;
   }
+
   public static final double maxSpeed = 7;
 
-  public static final int kMotorPort = 0;
+  public static final int kMotorPort       = 0;
   public static final int kEncoderAChannel = 0;
   public static final int kEncoderBChannel = 1;
-  public static final int kJoystickPort = 0;
+  public static final int kJoystickPort    = 0;
 
-  public static class targetingConstants{
+  public static class targetingConstants
+  {
+
     public static final double positiveScootch = Units.inchesToMeters(5);
     public static final double negitiveScootch = Units.inchesToMeters(-5);
-    public static final double scootchBack = Units.inchesToMeters(12);
+    public static final double scootchBack     = Units.inchesToMeters(12);
   }
-  
 
-  public static class ArmConstants{
-  public static int coralArmMotorID = 14;
-  public static int algaeArmMotorID = 15;
-  
-  public static final String kArmPositionKey = "ArmPosition";
-  public static final String kArmPKey = "ArmP";
+  public static class WristConstants
+  {
 
-  // The P gain for the PID controller that drives this arm.
-  public static final double kDefaultArmKp = 80.0;
-  public static final double kArmKi = 0.0;
-  public static final double kArmKd = 0.5;
+    public static final double kWristMomentOfInertia = 0.00032; // kg * m^2
+    public static final double kWristGearRatio       = 1.0;
 
-  // distance per pulse = (angle per revolution) / (pulses per revolution)
-  //  = (2 * PI rads) / (4096 pulses)
-  //public static final double kArmEncoderDistPerPulse = 2.0 * Math.PI / 4096;
+    public static class RollerConstants
+    {
 
-  public static final double kArmReduction = 200;
-  public static final double kArmMass = 8.0; // Kilograms 
-  public static final double kArmLength = Units.inchesToMeters(30);
-  public static final double kMinAngleRads = Units.degreesToRadians(-75);
-  public static final double kMaxAngleRads = Units.degreesToRadians(255);
-  
-  public static final double kArmkS = 0.0; // volts (V)
-  public static final double kArmkG = 0.762; // volts (V)
-  public static final double kArmkV = 0.762; // volt per velocity (V/(m/s))
-  public static final double kArmkA = 0.0; // volt per acceleration (V/(m/s²))
-
-  public static final double kAngleAllowableError = 1;//degree, for testing whether it's aroundAngle
-
-  
+      public static final double kWristMomentOfInertia = 0.00032; // kg * m^2
+      public static final double kWristGearRatio       = 1.0;
+    }
   }
-  
+
+
+  public static class ArmConstants
+  {
+
+    public static int coralArmMotorID = 14;
+    public static int algaeArmMotorID = 15;
+
+    public static final String kArmPositionKey = "ArmPosition";
+    public static final String kArmPKey        = "ArmP";
+
+    // The P gain for the PID controller that drives this arm.
+    public static final double kDefaultArmKp = 80.0;
+    public static final double kArmKi        = 0.0;
+    public static final double kArmKd        = 0.5;
+
+    // distance per pulse = (angle per revolution) / (pulses per revolution)
+    //  = (2 * PI rads) / (4096 pulses)
+    //public static final double kArmEncoderDistPerPulse = 2.0 * Math.PI / 4096;
+
+    public static final double kArmReduction = 200;
+    public static final double kArmMass      = 8.0; // Kilograms
+    public static final double kArmLength    = Units.inchesToMeters(30);
+    public static final double kMinAngleRads = Units.degreesToRadians(-75);
+    public static final double kMaxAngleRads = Units.degreesToRadians(255);
+
+    public static final double kArmkS = 0.0; // volts (V)
+    public static final double kArmkG = 0.762; // volts (V)
+    public static final double kArmkV = 0.762; // volt per velocity (V/(m/s))
+    public static final double kArmkA = 0.0; // volt per acceleration (V/(m/s²))
+
+    public static final double kAngleAllowableError = 1;//degree, for testing whether it's aroundAngle
+
+
+  }
+
 
   public static class ElevatorConstants
   {
+
     public static int elevatorMotorID = 13;
 
     public static final double kElevatorKp = 5;
@@ -80,12 +104,12 @@ public final class Constants {
     public static final double kElevatorkV = 0.762; // volt per velocity (V/(m/s))
     public static final double kElevatorkA = 0.0; // volt per acceleration (V/(m/s²))
 
-    public static final double kElevatorGearing = 10.0;
+    public static final double kElevatorGearing    = 10.0;
     public static final double kElevatorDrumRadius = Units.inchesToMeters(2.0);
-    public static final double kCarriageMass = 4.0; // kg
+    public static final double kCarriageMass       = 4.0; // kg
 
 
-   // Encoder is reset to measure 0 at the bottom, so minimum height is 0.
+    // Encoder is reset to measure 0 at the bottom, so minimum height is 0.
     public static final double kMinElevatorHeightMeters = 0;//min height / 10
     public static final double kMaxElevatorHeightMeters = 10.25;
 
@@ -93,19 +117,22 @@ public final class Constants {
     //public static final double kElevatorMaxAcceleration = 2.5;
 
     public static final double kElevatorAllowableError = 1;
-    public static final double kLowerToScoreHeight = Units.inchesToMeters(6);
+    public static final double kLowerToScoreHeight     = Units.inchesToMeters(6);
   }
-  public static class IntakeConstants{
-    public static final double AlgaeIntakeSpeeds = 0.8;
+
+  public static class IntakeConstants
+  {
+
+    public static final double AlgaeIntakeSpeeds  = 0.8;
     public static final double AlgaeOuttakeSpeeds = -0.8;
 
-    public static final int coralWristMotorID = 16;
-    public static final int coralRollerMotorID = 17;
+    public static final int    coralWristMotorID   = 16;
+    public static final int    coralRollerMotorID  = 17;
     public static final double defaultrRollerSpeed = 0;
-    public static final double kIntakeReduction = 0;
-    public static double kMinAngleRads;
-    public static double kMaxAngleRads;
-    public static double kIntakeLength;
+    public static final double kIntakeReduction    = 0;
+    public static       double kMinAngleRads;
+    public static       double kMaxAngleRads;
+    public static       double kIntakeLength;
   }
 
 }
--- src/main/java/frc/robot/subsystems/CoralIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralIntakeSubsystem.java
@@ -1,55 +1,105 @@
 package frc.robot.subsystems;
 
 
+import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.sim.SparkAbsoluteEncoderSim;
+import com.revrobotics.sim.SparkMaxSim;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
-
 import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.BatterySim;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+import edu.wpi.first.wpilibj.simulation.RoboRioSim;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.ArmConstants;
 import frc.robot.Constants.IntakeConstants;
+import frc.robot.Constants.WristConstants;
+import frc.robot.Constants.WristConstants.RollerConstants;
 
-public class CoralIntakeSubsystem extends SubsystemBase {
+public class CoralIntakeSubsystem extends SubsystemBase
+{
 
-        private final DCMotor m_wristGearbox = DCMotor.getNEO(3);
+  private final DCMotor m_wristGearbox = DCMotor.getNEO(3);
 
-    private final SparkMax m_wrist = new SparkMax(IntakeConstants.coralWristMotorID, MotorType.kBrushless);
-    private final SparkMax m_coralRollers = new SparkMax(IntakeConstants.coralRollerMotorID, MotorType.kBrushless);
-    
-    private final SparkClosedLoopController rollerController = m_coralRollers.getClosedLoopController();
-    private final SparkClosedLoopController wristController = m_wrist.getClosedLoopController();
-    private final RelativeEncoder m_encoder = m_wrist.getEncoder();
+  private final SparkMax m_wristMotor  = new SparkMax(IntakeConstants.coralWristMotorID, MotorType.kBrushless);
+  private final SparkMax m_rollerMotor = new SparkMax(IntakeConstants.coralRollerMotorID, MotorType.kBrushless);
+
+  private final SparkClosedLoopController wristController = m_wristMotor.getClosedLoopController();
+  private final RelativeEncoder           m_wristEncoder  = m_wristMotor.getEncoder();
+  private final AbsoluteEncoder           m_absEncoder    = m_wristMotor.getAbsoluteEncoder();
+
+  // Simulation stuff
+  private final DCMotor                 m_wristMotorGearbox  = DCMotor.getNEO(1);
+  private final DCMotor                 m_rollerMotorGearbox = DCMotor.getNEO(1);
+  private final FlywheelSim             m_wristSim           = new FlywheelSim(
+      LinearSystemId.createFlywheelSystem(
+          m_wristMotorGearbox,
+          WristConstants.kWristMomentOfInertia,
+          WristConstants.kWristGearRatio),
+      m_wristMotorGearbox,
+      1.0 / 4096.0);
+  private final FlywheelSim             m_rollerSim          = new FlywheelSim(LinearSystemId.createFlywheelSystem(
+      m_rollerMotorGearbox,
+      RollerConstants.kWristMomentOfInertia,
+      RollerConstants.kWristGearRatio), m_rollerMotorGearbox, 1.0 / 4096.0);
+  private final SparkAbsoluteEncoderSim m_wristAbsEncoderSim = new SparkAbsoluteEncoderSim(m_wristMotor);
+  private final SparkMaxSim             m_wristMotorSim      = new SparkMaxSim(m_wristMotor, m_wristMotorGearbox);
+  private final SparkMaxSim             m_rollerMotorSim     = new SparkMaxSim(m_rollerMotor, m_rollerMotorGearbox);
 
 
-    public CoralIntakeSubsystem() {
-     final SingleJointedArmSim m_wrist =
-            new SingleJointedArmSim(
-                    m_wristGearbox,
-                    IntakeConstants.kIntakeReduction,
-                    SingleJointedArmSim.estimateMOI(IntakeConstants.kIntakeLength, ArmConstants.kArmMass),
-                    IntakeConstants.kMinAngleRads,
-                    IntakeConstants.kMaxAngleRads,
-                    IntakeConstants.kIntakeLength,
-                    false,
-                    0,
-                    0.02 / 4096.0,
-                    0.0// Add noise with a std-dev of 1 tick
-            );
-    }
+  public CoralIntakeSubsystem()
+  {
+
+  }
 
 
-    public Command setCoralIntakeRoller(double speed){
-        return run(()->{
-            m_coralRollers.set(speed * IntakeConstants.defaultrRollerSpeed);
-        });
-    }
+  @Override
+  public void simulationPeriodic()
+  {
+    // In this method, we update our simulation of what our arm is doing
+    // First, we set our "inputs" (voltages)
+    m_wristSim.setInput(m_wristMotorSim.getAppliedOutput() * RoboRioSim.getVInVoltage());
+    m_rollerSim.setInput(m_rollerMotorSim.getAppliedOutput() * RoboRioSim.getVInVoltage());
 
-    public Command setWristAngle(double angle){
-        return run(()->{});
-    }
+    // Next, we update it. The standard loop time is 20ms.
+    m_wristSim.update(0.02);
+    m_rollerSim.update(0.02);
+
+    // Finally, we set our simulated encoder's readings and simulated battery voltage
+    //m_encoderSim.setDistance(m_coralArmSim.getAngleRads());
+    m_wristMotorSim.iterate(m_wristSim.getAngularVelocityRPM(),
+        RoboRioSim.getVInVoltage(), // Simulated battery voltage, in Volts
+        0.02); // Time interval, in Seconds
+
+    m_wristMotorSim.iterate(m_wristSim.getAngularVelocityRPM(),
+                            RoboRioSim.getVInVoltage(), // Simulated battery voltage, in Volts
+                            0.02);
+    m_rollerMotorSim.iterate(m_rollerSim.getAngularVelocityRPM(),RoboRioSim.getVInVoltage(), // Simulated battery voltage, in Volts
+                             0.02);
+
+    // SimBattery estimates loaded battery voltages
+    RoboRioSim.setVInVoltage(
+        BatterySim.calculateDefaultBatteryLoadedVoltage(m_rollerSim.getCurrentDrawAmps()+m_wristSim.getCurrentDrawAmps()));
+
+    // Update the Mechanism Arm angle based on the simulated arm angle
+    m_wristAbsEncoderSim.setPosition(m_wristEncoder.getPosition());
+
+  }
+
+  public Command setCoralIntakeRoller(double speed)
+  {
+    return run(() -> {
+      m_rollerMotor.set(speed * IntakeConstants.defaultrRollerSpeed);
+    });
+  }
+
+  public Command setWristAngle(double angle)
+  {
+    return run(() -> {
+    });
+  }
 }
 
